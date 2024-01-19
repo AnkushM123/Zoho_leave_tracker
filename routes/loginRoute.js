c60f6548b62df5d9c420d41fff4d2e6a922b859c
AnkushM123 <ankushm@incubxperts.com>
2024-01-19 12:02:42 +0530

    changed_role_api

--- routes/loginRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginUser = require('../apis/login');
-
-router.route("/").post(loginUser.login);
-
-module.exports = router; 
-
